1e7e56d5cd0b0717ee7f45d683ecf21aa04bd90f
charinass <cha_sor@yahoo.co.uk>
2020-12-11 23:54:41 +0100

    added templates, init, models

---
+++ Application/Models/models.py
@@ -0,0 +1,77 @@
+from Application import db
+
+
+class TTN_User(db.Model):
+    __tablename__ = "TTN_User"
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+    broker = db.Column(db.String)
+    topic = db.Column(db.String)
+
+    def __repr__(self) -> str:
+        return f"<TTN_User {self.user_id}>"
+
+
+class Device(db.Model):
+    __tablename__ = "Device"
+    dev_id = db.Column(db.Integer, primary_key=True)
+    device_name = db.Column(db.String)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    altitude = db.Column(db.Float)
+    location = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("TTN_User.user_id"))
+    users = db.relationship("TTN_User", backref="user", uselist=False)
+
+    def __repr__(self) -> str:
+        return f"<Device {self.dev_id}>"
+
+
+class Service(db.Model):
+    __tablename__ = "Service"
+    service_id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.String)
+    status = db.Column(db.Integer)
+    water_ml = db.Column(db.Integer)
+    countdown_timer = db.Column(db.Integer)
+    water_counter = db.Column(db.Integer)
+    voltage_max = db.Column(db.Float)
+    voltage_min = db.Column(db.Float)
+    current_max = db.Column(db.Float)
+    current_min = db.Column(db.Float)
+    dev_id = db.Column(db.Integer, db.ForeignKey("Device.dev_id"))
+    devices = db.relationship("Device", backref="device", uselist=False)
+
+    def __repr__(self) -> str:
+        return f"<Service {self.service_id}>"
+
+
+class Gateway(db.Model):
+    __tablename__ = "Gateway"
+    gateway_id = db.Column(db.Integer, primary_key=True)
+    gtw_id = db.Column(db.String)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    altitude = db.Column(db.Float)
+    location = db.Column(db.String)
+
+    def __repr__(self) -> str:
+        return f"<Gateway {self.gateway_id}>"
+
+
+class Connection(db.Model):
+    __tablename__ = "Connection"
+    conn_id = db.Column(db.Integer, primary_key=True)
+    dev_id = db.Column(db.Integer, db.ForeignKey("Device.dev_id"))
+    service_id = db.Column(db.Integer, db.ForeignKey("Service.service_id"))
+    gateway_id = db.Column(db.Integer, db.ForeignKey("Gateway.gateway_id"))
+    rssi = db.Column(db.Float)
+    snr = db.Column(db.Float)
+
+    devices = db.relationship("Device", backref="device", uselist=False)
+    services = db.relationship("Service", backref="service", uselist=False)
+    gateways = db.relationship("Gateway", backref="gateway", uselist=False)
+
+    def __repr__(self) -> str:
+        return f"<Connection {self.conn_id}>"
--- Application/__init__.py
+++ Application/__init__.py
@@ -1,17 +1,21 @@
 from flask import Flask
-from .Models import db
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_utils import database_exists
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = (
-        "sqlite:///" + "Application/Models/sqlite.db"
-    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + "sqlite.db"
 
-    db.init_app(app)
-    db.create_all()
+    db = SQLAlchemy(app)
+
+    if database_exists("sqlite:///" + "sqlite.db"):
+        db.init_app(app)
+    else:
+        db.init_app(app)
+        db.create_all()
 
     return app
 
