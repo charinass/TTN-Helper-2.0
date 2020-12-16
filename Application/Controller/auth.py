b54a3c6ea1f01155674c361fc2c455ef7c33197e
charinass <cha_sor@yahoo.co.uk>
2020-12-16 13:54:40 +0100

    auth edit and validate user

--- Application/Controller/auth.py
+++ Application/Controller/auth.py
@@ -2,22 +2,25 @@ import paho.mqtt.client as mqtt
 
 
 class Authentication:
-    def __init__(self, username, passphrase):
-        self.username = username
-        self.passphrase = passphrase
-        self.is_user_valid = True
+    """
+    To make sure that each user who registers has an actual account in TTN.
+    Call validate_user() with parameters of username and passphrase created in TTN.
+    """
+
+    def __init__(self):
+        self.the_broker = "eu.thethings.network"
+        self.the_topic = "+/devices/+/up"
         self.client = mqtt.Client()
 
-    def validate_user(self):
-        try:
-            self.client.username_pw_set(self.username, password=self.passphrase)
-            self.client.on_connect = self.__on_connect
-            self.client.connect("random", 1883, 60)
-        except:
-            self.is_user_valid = False
-            print(self.is_user_valid)
+    def validate_user(self, username, passphrase):
+        self.username = username
+        self.passphrase = passphrase
+        self.client.username_pw_set(self.username, password=self.passphrase)
+        self.client.on_connect = self.on_connect
+        self.client.connect(self.the_broker, 1883, 60)
+        self.client.loop_forever()
 
-    def __on_connect(self, client, userdata, flags, rc):
+    def on_connect(self, client, userdata, flags, rc):
         """
         The value of rc determines success or not:
         0: Connection successful
@@ -28,8 +31,11 @@ class Authentication:
         5: Connection refused - not authorised
         6-255: Currently unused.
         """
+        self.rc_checker = rc
         print("Connected to ", client._host, "port: ", client._port)
         print("Flags: ", flags, "return code: ", rc)
-        if rc >= 1 and rc <= 5:
-            self.is_user_valid = False
-            print("Failed to connect.")
+        if self.rc_checker > 0 and self.rc_checker < 6:
+            print("Account not found")
+            self.client.disconnect()
+        else:
+            self.client.subscribe(self.the_topic)
--- Application/Views/index.py
+++ Application/Views/index.py
@@ -1,7 +1,22 @@
-from flask import render_template
+from flask import render_template, request
+from werkzeug.utils import redirect
+
 from Application import app
+from ..Controller.auth import Authentication
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        username = request.form["username"]
+        passphrase = request.form["ttn_passphrase"]
+        a = Authentication()
+        a.validate_user(username, passphrase)
+        if a.rc_checker > 0 and a.rc_checker < 6:
+            return render_template(
+                "index.html", title="TTN Helper", response="Invalid user."
+            )
+        else:
+            return "logged in"
+
     return render_template("index.html", title="TTN Helper")
