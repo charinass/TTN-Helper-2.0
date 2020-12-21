a85d11d707f98c82be6b850b8c300b242bd770b3
charinass <cha_sor@yahoo.co.uk>
2020-12-21 01:58:43 +0100

    updates

--- Application/Views/dashboard_view.py
+++ Application/Views/dashboard_view.py
@@ -1,12 +1,14 @@
-from flask import render_template, abort, redirect, url_for, session, request
+from flask import render_template, redirect, url_for, session, make_response
 
 from Application import app
 from ..Controller import connect_mqtt
 
 
-@app.route("/dashboard/")
-def logged_in():
-    return render_template("dashboard.html", title="Dashboard")
+@app.route("/<username>/")
+def logged_in(username):
+    resp = make_response(render_template("dashboard.html", username=username))
+    resp.set_cookie("sessionID", username, expires=0)
+    return resp
 
 
 @app.route("/logout/")
